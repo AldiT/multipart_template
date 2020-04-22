d0d4bba2035ca0f3a02a03d0175015aa48760aef
Topalli Aldi (IFAG PMM SIS SWT SWI) <Aldi.Topalli@infineon.com>
2020-04-22 12:49:36 +0200

    initial commit

---
+++ flask_example.py
@@ -0,0 +1,24 @@
+"""
+Simple example for multipart processing © Aldi Topalli
+"""
+from flask import Flask, request
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    username = request.args.get('username')
+    if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename)) # Or do some arbitrary processing
+
+    ## Maybe get some other parameters using requests
+    
+    #### Question
+    ## How to reproduce this simple multipart processing in an OpenFaaS function?
+    return "File saved successfully"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
